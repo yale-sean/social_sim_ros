412a1ed8574fc6db1679f2c2ac4c98fd1356b5e7
Nathan <nathan@vertile.com>
2020-07-09 15:50:39 -0400

    some changes to csv writing in the trial runner

--- social_sim_ros/src/trial_runner.py
+++ social_sim_ros/src/trial_runner.py
@@ -9,25 +9,17 @@ from social_sim_ros.msg import TrialStart, PoseArray, TrialInfo
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionGoal
 from std_msgs.msg import Bool
 from random import randint
-import csv
+import csv, os
 
 class SocialSimRunner(object):
     def __init__(self):
         rospy.init_node("social_sim_runner")
-        self.prev_info_stamp = 0
-        self.info_stamp = 1
-        self.current_trial = 0
-        self.is_trialing = False
-        self.trial_ready = False
-        self.info_ready = False
-        self.positions_sub = rospy.Subscriber("/social_sim/spawn_positions", PoseArray, self.positions_callback, queue_size=10)
-        self.start_pub = rospy.Publisher("/social_sim/start_trial", TrialStart, queue_size=10)
-        self.status_sub = rospy.Subscriber("/social_sim/is_running", Bool, self.status_callback, queue_size=10)
-        self.info_sub = rospy.Subscriber("/social_sim/last_info", TrialInfo, self.info_callback, queue_size=10)
 
-        self.move_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.move_client.wait_for_server()
+        # call to restart the trial runner
+        self.reset_state()
 
+        self.output_folder = rospy.get_param('~output_folder', 'experiments')
+        print("Output folder: {}".format(self.output_folder))
         self.num_trials = rospy.get_param('~num_trials', 10)
         print("Number of Trials: {}".format(self.num_trials))
         self.num_peds = rospy.get_param('~num_peds', 10)
@@ -37,16 +29,28 @@ class SocialSimRunner(object):
         self.trial_name = rospy.get_param('~trial_name')
         print("Trial name: {}".format(self.trial_name))
 
-        file = open(self.trial_name + '.csv', 'w')
-        fnames = ['timestamp', 'dist_to_target', 'dist_to_ped',
-                  'num_collisions', 'run_complete', 'time_elapsed']
-        self.writer = csv.DictWriter(file, fieldnames=fnames)
-        self.writer.writeheader()
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+
+        self.positions_sub = rospy.Subscriber("/social_sim/spawn_positions", PoseArray, self.positions_callback, queue_size=10)
+        self.start_pub = rospy.Publisher("/social_sim/start_trial", TrialStart, queue_size=10)
+        self.status_sub = rospy.Subscriber("/social_sim/is_running", Bool, self.status_callback, queue_size=10)
+        self.info_sub = rospy.Subscriber("/social_sim/last_info", TrialInfo, self.info_callback, queue_size=10)
+
+        self.move_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.move_client.wait_for_server()
 
         print("Waiting for a /social_sim/is_running message")
         print("Please start Unity")
 
         rospy.spin()
+
+    def reset_state(self):
+        self.rows_to_write = []
+        self.prev_info_stamp = 0
+        self.info_stamp = 1
+        self.current_trial = 0
+        self.trial_ready = False
 
     def positions_callback(self, positions_msg):
         positions = positions_msg.positions
@@ -59,53 +63,72 @@ class SocialSimRunner(object):
         self.trial_ready = True
 
     def status_callback(self, trial_status_msg):
-        self.is_trialing = trial_status_msg.data
-        if self.is_trialing is False \
-        and self.info_stamp != self.prev_info_stamp \
-        and self.trial_ready is True:
-            if self.info_ready:
-                self.record_csv()
+        if not self.trial_ready:
+            return
+        # boolean message indicates if unity is running
+        is_trialing = trial_status_msg.data
+        if not is_trialing and self.info_stamp != self.prev_info_stamp:
             self.run_trial()
-            self.move_client.send_goal(self.goal)
-            self.prev_info_stamp = self.info_stamp
-        if self.trial_ready is True:
-            self.start_pub.publish(self.trial_start_msg)
-            #self.goal_pub.publish(self.goal_msg)
-            
-
 
     def info_callback(self, trial_info_msg):
-        self.trial_info = trial_info_msg
-        self.info_stamp = self.trial_info.header.stamp.secs
-        self.info_ready = True
+        self.record_row(trial_info_msg)
 
     def run_trial(self):
         print("Running Trial {}".format(self.current_trial))
         if self.current_trial >= self.num_trials:
+            print("Trials complete")
+            self.record_csv()
+            # TODO: reset to start state
+            # self.is_trialing = False
+            print("Exiting")
+            rospy.signal_shutdown("Trials Complete")
             return
-        self.trial_start_msg = TrialStart()
-        self.trial_start_msg.header.stamp = rospy.Time.now()
-        self.trial_start_msg.spawn = self.spawn_pos
-        self.trial_start_msg.target = self.target_pos
-        self.trial_start_msg.num_peds = self.num_peds
-        self.trial_start_msg.time_limit = self.time_limit
-        self.goal = MoveBaseGoal()
-        self.goal.target_pose.header.frame_id = "map"
-        self.goal.target_pose.header.stamp = rospy.Time.now()
-        self.goal.target_pose.pose = self.target_pos
 
+        # trial is starting, publish message
+        trial_start_msg = TrialStart()
+        trial_start_msg.header.stamp = rospy.Time.now()
+        trial_start_msg.spawn = self.spawn_pos
+        trial_start_msg.target = self.target_pos
+        trial_start_msg.num_peds = self.num_peds
+        trial_start_msg.time_limit = self.time_limit
+        self.start_pub.publish(trial_start_msg)
+
+        # send goal
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = self.target_pos
+        self.move_client.send_goal(goal)
+
+        # record previous stamp
+        self.prev_info_stamp = self.info_stamp
         self.current_trial += 1
 
-    def record_csv(self):
-        self.writer.writerow({
-                'timestamp': self.trial_info.header.stamp,
-                'dist_to_target': self.trial_info.dist_to_target,
-                'dist_to_ped': self.trial_info.dist_to_ped,
-                'num_collisions': self.trial_info.num_collisions,
-                'run_complete': self.trial_info.run_complete,
-                'time_elapsed': self.trial_info.time_elapsed
-            })
+    def record_row(self, msg):
+        self.info_stamp = msg.header.stamp.secs
+        row = {
+            'timestamp': msg.header.stamp,
+            'dist_to_target': msg.dist_to_target,
+            'dist_to_ped': msg.dist_to_ped,
+            'num_collisions': msg.num_collisions,
+            'run_complete': msg.run_complete,
+            'time_elapsed': msg.time_elapsed
+        }
+        self.rows_to_write.append(row)
 
+    def record_csv(self):
+        if len(self.rows_to_write) <= 0:
+            print("ERROR: No rows to write")
+            return
+        fieldnames = self.rows_to_write[0].keys()
+        csv_path = os.path.join(self.output_folder, '{}_{}.csv'.format(self.trail_name, self.rows_to_write[0]['timestamp']))
+        with open(csv_path, 'w') as f:
+            self.writer = csv.DictWriter(file)
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in self.rows_to_write:
+                writer.writerow(row)
+        print("Write csv {}".format(csv_path))
 
 if __name__ == "__main__":
     try:
